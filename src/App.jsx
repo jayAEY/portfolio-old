33ad2bc587d336de7955996d6b1a633310e732c7
jayAEY <106685702+jayAEY@users.noreply.github.com>
2023-10-06 16:45:11 -0700

    Add files via upload
    - cleaning up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Projects from './projects'
-import Project from './project'
-import ProjectExpanded from './projectExpanded'
-import { FaCodepen, FaEnvelope, FaGithub } from 'react-icons/fa'
+// import "./App.css";
+import Projects from "./projects";
+import { FaCodepen, FaEnvelope, FaGithub } from "react-icons/fa";
 
 function App() {
-  const[clicked, setClicked] = useState("")
+  const [clicked, setClicked] = useState("");
+
+  function lineAnimation(elem) {
+    let range = document.createRange();
+    range.selectNodeContents(elem);
+    let width = range.getBoundingClientRect().width;
+    elem.style.setProperty("--lineWidth", `${width}px`);
+  }
+
   return (
     <>
       <header>
-        <h1 onClick={() => setClicked("")}> Jo <br/> Apuya </h1> 
+        <h1
+          onClick={() => setClicked("")}
+          onMouseOver={(e) => lineAnimation(e.target)}
+        >
+          Jo <br /> Apuya
+        </h1>
         <div id="bio">
-          <p> I'm a freelance self-taught front-end web developer and graphic designer based in Vancouver, Canada</p> 
-          <div id='links'>
-            <a href='mailto:jomariapuya@gmail.com?subject = Questions&body = Message'><FaEnvelope/></a>
-            <a href='https://github.com/jayAEY'><FaGithub/></a>
-            <a href='https://codepen.io/jayAEY/'><FaCodepen/></a>
+          <p>
+            {" "}
+            I'm a freelance self-taught front-end web developer and graphic
+            designer based in Vancouver, Canada
+          </p>
+          <div id="links">
+            <a href="mailto:jomariapuya@gmail.com?subject = Questions&body = Message">
+              <FaEnvelope />
+            </a>
+            <a
+              href="https://github.com/jayAEY"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://codepen.io/jayAEY/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaCodepen />
+            </a>
           </div>
         </div>
       </header>
-      <Projects clicked={clicked} setClicked={setClicked}/>
+      <Projects
+        lineAnimation={lineAnimation}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
     </>
-  )
+  );
 }
-export default App
+export default App;
--- src/projects.jsx
+++ src/projects.jsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import Project from './project'
-import ProjectExpanded from "./projectExpanded"
-import data from "./projects-info.json"
+import Project from "./project";
+import ProjectExpanded from "./projectExpanded";
+import data from "./projects-info.json";
 
-const Projects = ( props ) => {
-  const{clicked, setClicked} = props
+const Projects = (props) => {
+  const { lineAnimation, clicked, setClicked } = props;
 
   function projectClick(elem) {
-    if(clicked == "") {
-      let elemProps = { "name" : elem.getAttribute("name"),
-                        "desc" : elem.getAttribute("desc"),
-                        "img" : elem.getAttribute("img"),
-                        "liveurl" : elem.getAttribute("liveurl"),
-                        "github" : elem.getAttribute("github")
-                      }
-      setClicked(elemProps)
+    if (clicked == "") {
+      let elemProps = {
+        name: elem.getAttribute("name"),
+        desc: elem.getAttribute("desc"),
+        mobileimage: elem.getAttribute("mobileImage"),
+        desktopimage: elem.getAttribute("desktopimage"),
+        liveurl: elem.getAttribute("liveurl"),
+        github: elem.getAttribute("github"),
+      };
+      setClicked(elemProps);
     } else {
-      setClicked("")
+      setClicked("");
     }
   }
-  let projectsReturn = Object.entries(data).map((e) => <Project projectClick={projectClick} name={e[1].name} key={e[1].name} desc={e[1].desc} img={e[1].img} liveurl={e[1].liveurl} github={e[1].github}/> )
 
-  let expandedReturn =  <ProjectExpanded projectClick={projectClick} name={clicked.name} desc={clicked.desc} img={clicked.img} liveurl={clicked.liveurl} github={clicked.github} />    
+  let projectsReturn = Object.entries(data).map((e) => (
+    <Project
+      projectClick={projectClick}
+      name={e[1].name}
+      key={e[1].name}
+      desc={e[1].desc}
+      mobileimage={e[1].mobileimage}
+      desktopimage={e[1].desktopimage}
+      liveurl={e[1].liveurl}
+      github={e[1].github}
+      lineAnimation={lineAnimation}
+    />
+  ));
 
-  return ( 
-    <section id="projects"> 
-      { clicked == "" ? projectsReturn : expandedReturn }
+  let expandedReturn = (
+    <ProjectExpanded
+      projectClick={projectClick}
+      name={clicked.name}
+      desc={clicked.desc}
+      mobileimage={clicked.mobileimage}
+      desktopimage={clicked.desktopimage}
+      liveurl={clicked.liveurl}
+      github={clicked.github}
+      lineAnimation={lineAnimation}
+    />
+  );
+
+  return (
+    <section id="projects">
+      {clicked == "" ? projectsReturn : expandedReturn}
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
