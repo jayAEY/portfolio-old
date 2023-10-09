372401cc94782ceed41181f7394a5262127d26cb
jayAEY <joapuya@gmail.com>
2023-10-09 10:51:07 -0700

    update various

--- src/project.jsx
+++ src/project.jsx
@@ -1,40 +1,54 @@
-const Project = ({ name, desc, img, liveurl, github, projectClick }) => {
+const Project = ({
+  name,
+  desc,
+  mobileimage,
+  desktopimage,
+  liveurl,
+  github,
+  projectClick,
+  lineAnimation,
+}) => {
+  function projectHover(elem, overOrOut) {
+    if (overOrOut == "over") {
+      elem.style.background = `
+      linear-gradient(rgba(0, 0, 0, 0.8),
+      rgba(0, 0, 0, 0.8)),
+      url("${mobileimage}")
+      `;
+      elem.style.backgroundPosition = "center";
+      elem.style.backgroundSize = "cover";
+    } else if (overOrOut == "out") {
+      elem.style.background = `url("${mobileimage}")`;
+      elem.style.backgroundPosition = "center";
+      elem.style.backgroundSize = "cover";
+    }
+  }
+  return (
+    <div
+      className="project"
+      id={name}
+      alt={`Screenshot of ${name}`}
+      style={{
+        background: `url("${mobileimage}")`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+      onMouseOver={(e) => {
+        projectHover(e.target, "over");
+        lineAnimation(e.target);
+      }}
+      onMouseOut={(e) => projectHover(e.target, "out")}
+      onClick={(e) => projectClick(e.target)}
+      name={name}
+      desc={desc}
+      mobileimage={mobileimage}
+      desktopimage={desktopimage}
+      liveurl={liveurl}
+      github={github}
+    >
+      <h2>{name}</h2>
+    </div>
+  );
+};
 
-    function projectHover(elem, overOrOut) {
-        if (overOrOut == "over") {
-                elem.style.background = `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url("${img}")`
-                elem.style.backgroundPosition = "center"
-                elem.style.backgroundSize = "cover"
-        } else if (overOrOut == "out") {
-                elem.style.background = `url("${img}")`
-                elem.style.backgroundPosition = "center"
-                elem.style.backgroundSize = "cover" 
-        }
-    } 
-
-    return (  
-        // <a href=".project-expand">        
-            <div 
-            className='project'
-            id={ name } 
-            alt={`Screenshot of ${name}`}
-            style={{ background: `url("${img}")`,
-                    backgroundPosition: "center",
-                    backgroundSize: "cover"
-                }} 
-            onMouseOver={(e) => projectHover(e.target, "over")} 
-            onMouseOut={(e) => projectHover(e.target, "out")}  
-            onClick={(e) => projectClick(e.target)}
-            name={name} 
-            desc={desc} 
-            img={img} 
-            liveurl={liveurl} 
-            github={github}
-            > 
-            <h2>{ name }</h2>
-            </div>
-        // </a> 
-    )
-}
-
-export default Project
+export default Project;
--- src/projectExpanded.jsx
+++ src/projectExpanded.jsx
@@ -1,23 +1,37 @@
-import { AiOutlineClose } from 'react-icons/ai'
-import { FaGithub, FaLink } from 'react-icons/fa' 
+import { AiOutlineClose } from "react-icons/ai";
+import { FaGithub, FaLink } from "react-icons/fa";
 
-const ProjectExpanded = ({ name, desc, img, liveurl, github, projectClick }) => {
+const ProjectExpanded = ({
+  projectClick,
+  name,
+  desc,
+  mobileimage,
+  desktopimage,
+  liveurl,
+  github,
+  lineAnimation,
+}) => {
   return (
-    <div className='project-expand'>
-      <AiOutlineClose id='close-button' onClick={() => projectClick()}/>
-      <h2>{name}</h2>
+    <div className="project-expand">
+      <AiOutlineClose id="close-button" onClick={() => projectClick()} />
+      <h2 onMouseOver={(e) => lineAnimation(e.target)}> {name}</h2>
       <p>{desc}</p>
-      <div className='project-links'>
-        <a href={github}><FaGithub/></a>
-        <a href={liveurl}><FaLink/></a>
+      <div className="project-links">
+        <a href={github} target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href={liveurl} target="_blank" rel="noopener noreferrer">
+          <FaLink />
+        </a>
       </div>
-      <div className='image-container' onClick={() => (window.open(liveurl))}>       
-        <img alt={`Screenshot of ${name}`}
-          src={img}>
-        </img> 
+      <div className="image-container" onClick={() => window.open(liveurl)}>
+        <img
+          alt={`Screenshot of ${name}`}
+          src={window.innerWidth < 500 ? mobileimage : desktopimage}
+        ></img>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectExpanded
+export default ProjectExpanded;
